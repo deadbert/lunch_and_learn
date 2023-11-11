f7450a030899694a5ed039fa0f367cdf0d1c7146
Nick Spencer <strangebrew202@gmail.com>
2023-11-11 14:03:12 -0700

    Add: added 201 response code for successful user creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::UsersController < ApplicationController
     user_info = JSON.parse(request.body.read, symbolize_names: true)
     user = User.new(name: user_info[:name], email: user_info[:email], password: user_info[:password], api_key: '1234')
     if user.save
-      render json: UserSerializer.new(user)
+      render json: UserSerializer.new(user), status: 201
     else
       render json: ErrorSerializer.new(Error.new(user.errors.full_messages))
     end
