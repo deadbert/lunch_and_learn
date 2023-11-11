a75a2234ee8cb785d1a304fb99c8cd42a2935e63
Nick Spencer <strangebrew202@gmail.com>
2023-11-11 14:12:07 -0700

    Add: used SecureRandom module to implement randomized api keys for new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
+require 'securerandom'
+
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password
   validates :api_key, uniqueness: true, presence: true
 
+  before_validation :set_api_key
+
   has_secure_password
+
+  private
+
+  def set_api_key
+    self.api_key = SecureRandom.base64
+  end
 end
