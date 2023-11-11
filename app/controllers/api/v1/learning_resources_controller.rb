1e5cc20d94ceceacac9cf855bd1db15a9f04c693
Nick Spencer <strangebrew202@gmail.com>
2023-11-11 10:43:40 -0700

    Add: LearningResourceSerializer and #set_search_term methods, endpoint now renders JSON for FE

--- app/controllers/api/v1/learning_resources_controller.rb
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -2,5 +2,7 @@ class Api::V1::LearningResourcesController < ApplicationController
 
   def index
     resources = LearningResourcesFacade.new.learning_resources(params[:country])
+    resources.set_search_term(request.params[:country])
+    render json: LearningResourceSerializer.new(resources)
   end
 end
--- app/poros/learning_resource.rb
+++ app/poros/learning_resource.rb
@@ -1,15 +1,19 @@
 class LearningResource
-
+  attr_reader :id, :country, :video, :images
   def initialize(video, images)
+    @id = nil
     @country = nil
     @video = {title: video[:snippet][:title], youtube_video_id: video[:id][:videoId]}
     @images = make_images(images)
-    require 'pry';binding.pry
   end
 
   def make_images(images)
     images.map do |image|
       { alt_tag: image[:alt_description], url: image[:urls][:small]}
     end
+  end
+
+  def set_search_term(search_term)
+    @country = search_term
   end
 end
