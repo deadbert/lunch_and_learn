fb037ee051d813ae47f598ab66d5078338e97345
Nick Spencer <strangebrew202@gmail.com>
2023-11-11 10:48:39 -0700

    Add: #make_video method in LearningResource PORO to catch edge case error that occoured when no videos were found for search

--- app/poros/learning_resource.rb
+++ app/poros/learning_resource.rb
@@ -3,13 +3,21 @@ class LearningResource
   def initialize(video, images)
     @id = nil
     @country = nil
-    @video = {title: video[:snippet][:title], youtube_video_id: video[:id][:videoId]}
+    @video = make_video(video)
     @images = make_images(images)
   end
 
   def make_images(images)
     images.map do |image|
       { alt_tag: image[:alt_description], url: image[:urls][:small]}
+    end
+  end
+
+  def make_video(video)
+    if !video.nil?
+      {title: video[:snippet][:title], youtube_video_id: video[:id][:videoId]}
+    else
+      {}
     end
   end
 
