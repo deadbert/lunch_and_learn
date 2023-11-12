2d0a5ed1a587a471b635143d9a4954fe7ced9d0e
Nick Spencer <strangebrew202@gmail.com>
2023-11-12 10:22:32 -0700

    Add: password match condition to user creation endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,9 @@ class Api::V1::UsersController < ApplicationController
   def create
     user_info = JSON.parse(request.body.read, symbolize_names: true)
     user = User.new(name: user_info[:name], email: user_info[:email], password: user_info[:password])
-    if user.save
+    if user_info[:password] != user_info[:password_confirmation]
+      render json: {error: "Passwords must match"}
+    elsif user.save
       render json: UserSerializer.new(user), status: 201
     else
       render json: ErrorSerializer.new(Error.new(user.errors.full_messages))
