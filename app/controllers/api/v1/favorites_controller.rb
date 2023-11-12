0470e856ed04fe6c54e5453b8c2a10ed6e352f67
Nick Spencer <strangebrew202@gmail.com>
2023-11-11 17:59:15 -0700

    Add: routes and actions for favoriting a recipe for a user

---
+++ app/controllers/api/v1/favorites_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::FavoritesController < ApplicationController
+
+  def create
+    parameters = JSON.parse(request.body.read, symbolize_names: true)
+    user = User.find_by(api_key: parameters[:api_key])
+    if user
+      Favorite.create(country: parameters[:country], recipe_link: parameters[:recipe_link], recipe_title: parameters[:recipe_title], user_id: user.id)
+      render json: {success: "Favorite added successfully"}
+    else
+      render json: ErrorSerializer.new(Error.new("No user found for api_key")), status: 404
+    end
+  end
+end
