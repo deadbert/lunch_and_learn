2598ddf82ec690b54117395dfeb5914770e745f2
Nick Spencer <strangebrew202@gmail.com>
2023-11-12 11:41:32 -0700

    Add: happy path spec test for get request to favorites endpoint for returning user favorited recipes

--- spec/api/v1/favorites/favorites_spec.rb
+++ spec/api/v1/favorites/favorites_spec.rb
@@ -36,4 +36,27 @@ RSpec.describe "Favorites endpoint" do
       expect(result[:data][:attributes][:error_message]).to eq("No user found for api_key")
     end
   end
+
+  describe "When a get request hit the favorites endpoint" do
+    it "returns a list of the users favorited recipes" do
+      bobby = User.create(name:"bobby", email: "hill@king.com", password: "1234", password_confirmation: "1234")
+
+      Favorite.create(country: "place", recipe_link: "linky", recipe_title: "food stuff", user_id: bobby.id)
+
+      get "/api/v1/favorites?api_key=#{bobby.api_key}"
+
+      result = JSON.parse(response.body, symbolize_names: true)
+
+      expect(result).to have_key :data
+      expect(result[:data]).to be_an Array
+      expect(result[:data].first).to have_key :id
+      expect(result[:data].first).to have_key :type
+      expect(result[:data].first[:type]).to eq("favorite")
+      expect(result[:data].first).to have_key :attributes
+      expect(result[:data].first[:attributes]).to have_key :recipe_title
+      expect(result[:data].first[:attributes]).to have_key :recipe_link
+      expect(result[:data].first[:attributes]).to have_key :country
+      expect(result[:data].first[:attributes]).to have_key :created_at
+    end
+  end
 end
