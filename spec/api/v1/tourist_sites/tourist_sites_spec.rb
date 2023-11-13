fd713853e3439d7ac853ef251f18c180f07d531b
Nick Spencer <strangebrew202@gmail.com>
2023-11-13 10:11:34 -0700

    Refactor: fix spec test to utilize vcr properly and fixed formating of expect tests to grab the correct data and keys

--- spec/api/v1/tourist_sites/tourist_sites_spec.rb
+++ spec/api/v1/tourist_sites/tourist_sites_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "Tourist sites endpoint" do
   describe "When a get request hits the tourist_sites endpoint" do
-    it "returns a collection of tourist sites within 100 meter radius of capital city" do
+    it "returns a collection of tourist sites within 100 meter radius of capital city", :vcr do
 
       get '/api/v1/tourist_sites?country=France'
 
       result = JSON.parse(response.body, symbolize_names: true)
 
       expect(result).to have_key :data 
-      expect(result[:data]).to have_key :id
-      expect(result[:data]).to have_key :type
-      expect(result[:data][:type]).to eq("tourist_side")
-      expect(result[:data]).to have_key :attributes
-      expect(result[:data][:attributes]).to have_key :name
-      expect(result[:data][:attributes]).to have_key :address
-      expect(result[:data][:attributes]).to have_key :place_id
+      expect(result[:data].first).to have_key :id
+      expect(result[:data].first).to have_key :type
+      expect(result[:data].first[:type]).to eq("tourist_site")
+      expect(result[:data].first).to have_key :attributes
+      expect(result[:data].first[:attributes]).to have_key :name
+      expect(result[:data].first[:attributes]).to have_key :address
+      expect(result[:data].first[:attributes]).to have_key :place_id
     end
   end
 end
