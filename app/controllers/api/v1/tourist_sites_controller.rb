88cc8581cf826c6117255c12a34c299fae71a202
Nick Spencer <strangebrew202@gmail.com>
2023-11-13 10:05:39 -0700

    Add: TouristSite PORO, and map tourist_sites from PlacesService to POROS, handed back to controller as TouristSite objects, serialize next

--- app/controllers/api/v1/tourist_sites_controller.rb
+++ app/controllers/api/v1/tourist_sites_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::TouristSitesController < ApplicationController
 
   def index
     tourist_sites = TouristSitesFacade.new.find_sites(params[:country])
+    require 'pry';binding.pry
   end
 end
--- app/facades/tourist_sites_facade.rb
+++ app/facades/tourist_sites_facade.rb
@@ -8,6 +8,8 @@ class TouristSitesFacade
   def find_sites(country)
     lat_long = @countries_service.country(country).first[:capitalInfo][:latlng]
     tourist_sites = @places_service.find_tourist_sites(lat_long[1], lat_long[0])
-    require 'pry';binding.pry
+    tourist_sites[:features].map do |site|
+      TouristSite.new(site)
+    end
   end
 end
