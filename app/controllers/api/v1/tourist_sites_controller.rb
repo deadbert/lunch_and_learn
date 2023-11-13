164d60ae2182f102e3257e902d110f2998565a4b
Nick Spencer <strangebrew202@gmail.com>
2023-11-13 10:08:52 -0700

    Add: TouristSiteSerializer created, now returning serialized response back from endpoint that matches requirements

--- app/controllers/api/v1/tourist_sites_controller.rb
+++ app/controllers/api/v1/tourist_sites_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::TouristSitesController < ApplicationController
 
   def index
     tourist_sites = TouristSitesFacade.new.find_sites(params[:country])
-    require 'pry';binding.pry
+    render json: TouristSiteSerializer.new(tourist_sites)
   end
 end
--- app/poros/tourist_site.rb
+++ app/poros/tourist_site.rb
@@ -1,7 +1,8 @@
 class TouristSite 
-  attr_reader :name, :address, :place_id
+  attr_reader :name, :address, :place_id, :id
 
   def initialize(data)
+    @id = nil
     @name = data[:properties][:name]
     @address = data[:properties][:formatted]
     @place_id = data[:properties][:place_id]
