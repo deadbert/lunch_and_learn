3babb517eff95feea61693601e7eaf514146e7b2
Nick Spencer <strangebrew202@gmail.com>
2023-11-13 09:09:40 -0700

    Add: route and controller for new tourist_sites get endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
       resources :users, only: [:create]
       resources :sessions, only: [:create]
       resources :favorites, only: [:create, :index]
+      resources :tourist_sites, only: [:index]
     end
   end
 end
