24e03f5a1cc76d6fd2ab2252b25689eed4f50ea5
Nick Spencer <strangebrew202@gmail.com>
2023-11-13 10:39:51 -0700

    Add: more sad path for when capital city lat/long is found, but no turist locations found, cleared up wording on sad path when searched country doesn't have lat/long for a capital city

--- spec/api/v1/tourist_sites/tourist_sites_spec.rb
+++ spec/api/v1/tourist_sites/tourist_sites_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe "Tourist sites endpoint" do
 
       result = JSON.parse(response.body, symbolize_names: true)
 
+      expect(result).to be_a Hash
       expect(result).to have_key :data 
       expect(result[:data].first).to have_key :id
       expect(result[:data].first).to have_key :type
@@ -18,13 +19,23 @@ RSpec.describe "Tourist sites endpoint" do
       expect(result[:data].first[:attributes]).to have_key :place_id
     end
 
-    it "returns a hash with error: No results found for search if no country is found from user input", :vcr do
+    it "returns a hash with error: No results found for search if country found does not have capital city lat/long values", :vcr do
 
       get '/api/v1/tourist_sites?country=antarctica'
 
       result = JSON.parse(response.body, symbolize_names: true)
       expect(result).to be_a Hash
       expect(result[:error]).to eq("No results found for search")
+    end
+
+    it "returns an empty array when a country is found, but no tourist_sites are found by Places API", :vcr do
+      get '/api/v1/tourist_sites?country=Uruguay'
+
+      result = JSON.parse(response.body, symbolize_names: true)
+
+      expect(result).to be_a Hash
+      expect(result).to have_key :data
+      expect(result[:data]).to be_empty
     end
   end
 end
